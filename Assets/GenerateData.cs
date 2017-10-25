0ab1a63d7d32924bb8551f37a0b9990b77deb944
Kronstrand <k.kronstrand@gmail.com>
2017-10-26 00:24:17 +0200

    initial commit

---
+++ Assets/GenerateData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GenerateData : MonoBehaviour {
+
+
+    int i = 1;
+    float timer = 0;
+    String filePath = "C:/Users/kronstrand/Documents/Data/data.csv";
+    String filePathTarget = "C:/Users/kronstrand/Documents/Data/target/target_";
+    string filePathInput = "C:/Users/kronstrand/Documents/Data/input/input_";
+
+
+    Camera playerCam;
+    Camera mapCam;
+
+
+    // Use this for initialization
+    void Start () {
+
+        playerCam = GameObject.Find("Player/MainCamera").GetComponent<Camera>();
+        mapCam = GameObject.Find("MapCamHolder/MapCam").GetComponent<Camera>();
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+    }
+
+    void LateUpdate()
+    {
+        if (timer > 0.2f)
+        {
+            StartCoroutine(saveScreen());
+            i++;
+            timer = 0;
+        }
+    }
+
+    public IEnumerator saveScreen()
+    {
+        yield return new WaitForEndOfFrame();
+
+        // set the sizes for the image to be captured
+        //int imgWidth = Convert.ToInt32(Math.Floor(Screen.width * 0.5f));
+        //int imgHeight = Convert.ToInt32(Math.Floor(Screen.height * 0.8f));
+
+        //take a screenshot and store as a texture
+        Texture2D tex = new Texture2D(playerCam.pixelWidth, playerCam.pixelHeight);
+        tex.ReadPixels(new Rect(playerCam.pixelRect.x, playerCam.pixelRect.y, playerCam.pixelWidth, playerCam.pixelHeight), 0, 0);
+        tex.Apply();
+
+        //save in image in csv
+        //File.AppendAllText(filePath, toStringCSV(tex.EncodeToJPG()));
+
+        //save the screenshot as jpg
+        File.WriteAllBytes(filePathTarget + i + ".jpg", tex.EncodeToJPG());
+
+
+        //take a screenshot and store as a texture
+        tex = new Texture2D(mapCam.pixelWidth, mapCam.pixelHeight);
+        tex.ReadPixels(new Rect(mapCam.pixelRect.x, mapCam.pixelRect.y, mapCam.pixelWidth, mapCam.pixelHeight), 0, 0);
+        tex.Apply();
+
+        //save the screenshot as jpg
+        File.WriteAllBytes(filePathInput + i + ".jpg", tex.EncodeToJPG());
+        
+    }
+}
---
+++ Assets/MapCam/MapCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCam : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update (){
+
+        this.transform.position = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);
+        this.transform.rotation = Quaternion.Euler(this.transform.eulerAngles.x, player.transform.eulerAngles.y, this.transform.eulerAngles.z);
+		
+	}
+}
